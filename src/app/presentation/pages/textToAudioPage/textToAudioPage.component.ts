90fc055cdf4dff15d288aea1fe15c084aa346f45
Nestor <nestor.edrev@gmail.com>
2024-08-27 13:46:49 +0200

    Llamar el END-POINT text-to-audio y visualizar por pantalla el mp3 generado mediante el tag audio de HTML

--- src/app/presentation/pages/textToAudioPage/textToAudioPage.component.ts
+++ src/app/presentation/pages/textToAudioPage/textToAudioPage.component.ts
@@ -1,13 +1,64 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ChatMessageComponent, MyMessageComponent, TypingLoaderComponent, textMessageBoxEvent, TextMessageBoxSelectComponent } from '@components/index';
+import { Message } from '@interfaces/message.interface';
+import { OpenAiService } from '@services/openai.service';
 
 @Component({
   selector: 'app-text-to-audio-page',
   standalone: true,
   imports: [
     CommonModule,
+    TextMessageBoxSelectComponent,
+    ChatMessageComponent,
+    MyMessageComponent,
+    TypingLoaderComponent,
   ],
   templateUrl: './textToAudioPage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class TextToAudioPageComponent { }
+export default class TextToAudioPageComponent {
+
+  public messages = signal<Message[]>([]);
+  public isLoading = signal<boolean>(false);
+  public voices = signal<{id:string, text:string}[]>(
+    [
+      {id: 'alloy', text: 'Alloy'},
+      {id: 'echo', text: 'Echo'},
+      {id: 'fable', text: 'Fable'},
+      {id: 'onyx', text: 'Onyx'},
+      {id: 'nova', text: 'Nova'},
+      {id: 'Shimmer', text: 'shimmer'}
+    ]
+  );
+  
+  public openAiService = inject(OpenAiService);
+
+  handleMessageWithSelect( { prompt, selectedOption }:textMessageBoxEvent){
+    
+    const message = `${selectedOption} - ${prompt}`
+
+    this.isLoading.set(true);
+    this.messages.update( prev => [
+      ...prev,
+      {
+        isGpt: false,
+        text: message
+      }
+    ])
+
+    this.openAiService.textToAudio(prompt, selectedOption)
+    .subscribe( ({message, audioUrl}) => {
+      this.isLoading.set(false);
+
+      this.messages.update( prev => [
+        ...prev,
+        {
+          isGpt: true,
+          text: message,
+          audioUrl: audioUrl
+        }
+      ])
+    })
+  }
+}
